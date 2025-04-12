50ee6c58c4b89cf31714168bea7667e3402a92ee
arthursiq5 <arthur.siq5@gmail.com>
2025-04-12 13:41:26 -0300

    Generated account

--- desafios/sintaxe/src/io/github/arthursiq5/src/Main.java
+++ desafios/sintaxe/src/io/github/arthursiq5/src/Main.java
@@ -4,12 +4,52 @@
  */
 package io.github.arthursiq5.src;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Arthur
  */
 public class Main {
+    private static void menuInicial() {
+        System.out.println("Bem-vindo ao nosso banco\n");
+        System.out.println("Por favor, selecione uma opção:");
+        System.out.println("1 - criar conta");
+        System.out.println("2 - listar contas existentes");
+        System.out.println("3 - apagar conta");
+        System.out.println("0 - sair");
+        System.out.println("\nSelecione uma opção:");
+    }
+    
+    private static void criarConta(Scanner sc) {
+        String nomeConta;
+        int agencia;
+        String conta;
+        double saldo;
+        
+        System.out.println("Digite seu nome completo:");
+        nomeConta = sc.nextLine();
+        System.out.println("Digite sua agência:");
+        agencia = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Digite sua conta:");
+        conta = sc.nextLine();
+        System.out.println("Digite seu saldo:");
+        saldo = sc.nextDouble();
+        sc.nextLine();
+        
+        ContaTerminal ct = new ContaTerminal(nomeConta, agencia, conta, saldo);
+        System.out.println(ct.criarContaMensagem());
+    }
+    
     public static void main(String[] args) {
-        // todo gerar classe
+        Scanner sc = new Scanner(System.in);
+        int opcao = 0;
+        do {            
+            menuInicial();
+            opcao = sc.nextInt();
+            sc.nextLine();
+            if (opcao == 1) criarConta(sc);
+        } while (opcao != 0);
     }
 }
